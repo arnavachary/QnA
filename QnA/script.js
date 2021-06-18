309287e23c4c8baf260f0072dfdb24a832a51fbc
arnavachary <77060770+arnavachary@users.noreply.github.com>
2021-06-18 20:02:13 +0530

    Add files via upload

---
+++ QnA/script.js
@@ -0,0 +1,20 @@
+function closeDiv1() {
+  console.log(document.getElementById("data1").style.display);
+  if (document.getElementById("data1").style.display == "none") {
+    document.getElementById("data1").style.display = "block";
+  } else document.getElementById("data1").style.display = "none";
+}
+
+function closeDiv2() {
+  console.log(document.getElementById("data2").style.display);
+  if (document.getElementById("data2").style.display == "none") {
+    document.getElementById("data2").style.display = "block";
+  } else document.getElementById("data2").style.display = "none";
+}
+
+function closeDiv3() {
+  console.log(document.getElementById("data3").style.display);
+  if (document.getElementById("data3").style.display == "none") {
+    document.getElementById("data3").style.display = "block";
+  } else document.getElementById("data3").style.display = "none";
+}
